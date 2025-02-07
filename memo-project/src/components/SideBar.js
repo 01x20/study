b7544664e986e5515f28281a49a5beaa05d8236e
kimgaeul <ge950120@gmail.com>
2025-02-07 17:47:19 +0900

    메모장 프로젝트 2단계
    선택된 요소 판단 소스 추가
    선택된 요소 스타일링 추가

--- memo-project/src/components/SideBar.js
+++ memo-project/src/components/SideBar.js
@@ -2,11 +2,15 @@ import MemoList from './MemoList';
 import SideBarHeader from './SideBarHeader';
 import SideBarFooter from './SideBarFooter';
 
-function SideBar({ memos, setSelectedMemoIndex }) {
+function SideBar({ memos, selectedMemoIndex, setSelectedMemoIndex }) {
   return (
     <div className="SideBar">
       <SideBarHeader />
-      <MemoList memos={memos} setSelectedMemoIndex={setSelectedMemoIndex} />
+      <MemoList
+        memos={memos}
+        selectedMemoIndex={selectedMemoIndex}
+        setSelectedMemoIndex={setSelectedMemoIndex}
+      />
       <SideBarFooter />
     </div>
   );
